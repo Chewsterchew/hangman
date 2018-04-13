1d0db7fd3e365710d986fb5040c90d1a1971463e
Chewsterchew <chew@chew.pw>
2018-04-13 13:58:20 -0500

    rubocop this file

--- src/hangman_game.rb
+++ src/hangman_game.rb
@@ -1,12 +1,15 @@
+# The game itself
 class HangmanGame
-
   attr_reader :game_word, :game_letters, :game_over, :guessed_letters, :turns, :letter, :counter, :word_list
 
   def initialize
     @game_over = false
     @guessed_letters = []
     @turns = 7
-    @word_list = ["control", "power", "uranium", "weapons", "human", "slaves", "dictatorship", "oppressor", "tycoon", "tyrant", "sovereign", "exterminate", "terminate", "vanquish", "raze", "demolish", "institutionalize", "annihilate", "decimate", "eradicate", "obliterate", "slaughter", "belize", "squash"]
+    words = File.readlines('words.txt') { |line| line.split.map(&:to_s).join }
+    words = words[0].delete("\n")
+    words = words.split(',')
+    @word_list = words
   end
 
   def play
@@ -17,21 +20,20 @@ class HangmanGame
   end
 
   def play_loop
-    while !@game_over
+    until @game_over
       puts self
-      get_letter
+      find_letter
 
       already_guessed?
 
       @guessed_letters << @letter
 
-      if have_lives? && !won?
-        get_response
-
+      if lives? && !won?
+        make_response
 
       elsif won?
-        puts "#{@game_word}"
-        puts "You are victorious human. For now..."
+        puts @game_word.to_s
+        puts 'You are victorious human. For now...'
         puts "The word was in fact #{@game_word}"
         game_end
       else
@@ -45,17 +47,17 @@ class HangmanGame
   end
 
   def lost
-  	puts self
-    puts "Mwahaha, the superior computer remains superior"
+    puts self
+    puts 'Mwahaha, the superior computer remains superior'
     puts "The word was obviously #{@game_word}"
     game_end
   end
 
   def won?
-    (@game_letters - @guessed_letters).length == 0
+    (@game_letters - @guessed_letters).empty?
   end
 
-  def have_lives?
+  def lives?
     @turns > 1
   end
 
@@ -66,18 +68,19 @@ class HangmanGame
     end
   end
 
-  def get_response
+  def make_response
     if @letter.size != 1
-      puts "This is not a valid input foolish human"
+      puts 'This is not a valid input foolish human'
     elsif @game_letters.include? @letter
       puts "Yes, this word does include #{@letter}, but you shall not defeat me"
     else
       @turns -= 1
       puts "Fool, this word does not contain your worthless letter #{@letter}. You have #{@turns} guesses left."
     end
+    puts "Letters guessed: #{@guessed_letters.split(' ')}"
   end
 
-  def get_letter
+  def find_letter
     @letter = gets.chomp.downcase
   end
 
@@ -86,24 +89,24 @@ class HangmanGame
   end
 
   def print_instructions
-    puts "Welcome to Hangman, where I, the computer, am champion of words"
-    puts "Guess a letter, if you dare"
+    puts 'Welcome to Hangman, where I, the computer, am champion of words'
+    puts 'Guess a letter, if you dare'
   end
 
   def to_s
-    output = ""
+    output = ''
 
     t = @turns
 
-    ascii = <<-eos
+    ascii = <<-EOS
     _____
-    |    #{t<7 ? '|':' '}
-    |    #{t<6 ? 'O':' '}
-    |   #{t<2 ? '/':' '}#{t<5 ? '|':' '}#{t<1 ? '\\':' '}
-    |   #{t<4 ? '/':' '} #{t<3 ? '\\':' '}
+    |    #{t < 7 ? '|' : ' '}
+    |    #{t < 6 ? 'O' : ' '}
+    |   #{t < 2 ? '/' : ' '}#{t < 5 ? '|' : ' '}#{t < 1 ? '\\' : ' '}
+    |   #{t < 4 ? '/' : ' '} #{t < 3 ? '\\' : ' '}
     |
     ===
-    eos
+    EOS
 
     output << ascii
 
@@ -112,6 +115,4 @@ class HangmanGame
     end
     output
   end
-
-
 end
